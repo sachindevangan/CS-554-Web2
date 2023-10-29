c6568ff32cf1791d8d1fd95cac0c59cb7d8b1d6b
Sachin Devangan <dsachin.98@gmail.com>
2023-10-29 16:39:03 -0400

    Paginationa ans 20 objects per page for search

--- Lab 4/Metropolitan-Museum-of-Art/src/components/Collection.jsx
+++ Lab 4/Metropolitan-Museum-of-Art/src/components/Collection.jsx
@@ -1,38 +1,68 @@
 // import React, { useState, useEffect } from 'react';
-// import { Link, useParams, useNavigate } from 'react-router-dom';
+// import { useNavigate, useParams , Link, useLocation } from 'react-router-dom';
 // import axios from 'axios';
 // import ArtListCard from './ArtListCard';
 // import { Grid } from '@mui/material';
 // import SearchArts from './SearchArts';
 // import './component.css';
 
-// const CollectionPage = () => {
+//   const CollectionPage = () => {
 //   const navigate = useNavigate();
-//   const { page } = useParams();
+//   const location = useLocation();
+//   const queryParams = new URLSearchParams(location.search);
+//   const departmentId = queryParams.get('departmentIds');
+//   const { page, departmentId: departmentIdParam } = useParams();
 //   const [objects, setObjects] = useState([]);
 //   const [loading, setLoading] = useState(true);
 //   const [totalPages, setTotalPages] = useState(1);
 //   const [currentPage, setCurrentPage] = useState(page ? parseInt(page) : 1);
 //   const [searchTerm, setSearchTerm] = useState('');
 
+  
+// const isDepartmentSearch = !!departmentId; 
+
+//   const pageTitle = isDepartmentSearch
+//   ? `Department ${departmentId} - Page ${currentPage}`
+//   : `Search Results for "${searchTerm}" - Page ${currentPage}`;
+
 //   useEffect(() => {
-//     console.log('Current Search Term:', searchTerm);
 //     const fetchObjectsForPage = async () => {
 //       try {
-//         setLoading(true);
-//         let apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects?offset=${(currentPage - 1) * 50}&limit=50`;
-//         if (searchTerm !== undefined && searchTerm !== null && searchTerm !== '') {
-//           apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${encodeURIComponent(searchTerm)}&limit=20`;
+//         if (currentPage <= 0) {
+//           navigate('/400'); 
+//           return;
 //         }
-//         console.log('SearchTerm URL', apiUrl)
-    
+
+//         if (currentPage > totalPages && totalPages > 1) {
+//           navigate('/404'); 
+//           return;
+//         }
+
+//         setLoading(true);
+//         let apiUrl = '';
+//         if (departmentId) {
+//         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}&offset=${(currentPage - 1) * 50}`;
+//         } else {
+//         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects?offset=${(currentPage - 1) * 50}`;
+//         if (searchTerm !== undefined && searchTerm !== null && searchTerm !== '') {
+//         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${(searchTerm)}`;
+//          }
+//         }
+
 //         const response = await axios.get(apiUrl);
+//         console.log("These are the objects", response.data)
 //         const allObjects = response.data.objectIDs;
+//         if (allObjects === null) {
+//           setTotalPages(0);
+//           setObjects([]);
+//           setLoading(false);
+//           return;
+//         }
 //         const totalObjects = allObjects.length;
 //         const calculatedTotalPages = Math.ceil(totalObjects / 50);
 //         setTotalPages(calculatedTotalPages);
 
-//         // Slice the object IDs for the current page
+      
 //         const startIndex = (currentPage - 1) * 50;
 //         const endIndex = Math.min(startIndex + 50, totalObjects);
 //         const objectsForCurrentPage = allObjects.slice(startIndex, endIndex);
@@ -45,72 +75,102 @@
 
 //         if (error.response && error.response.status === 404) {
 //           navigate('/404');
-//         }
-//         // Handle 400 error
-//         else if (error.response && error.response.status === 400) {
+//         } else if (error.response && error.response.status === 400) {
 //           navigate('/400');
 //         }
 //       }
 //     };
+//     fetchObjectsForPage();
+//   }, [currentPage, searchTerm, departmentId, navigate, totalPages,  isDepartmentSearch]);
 
-//     // Check if currentPage is below 1 or above totalPages
-//     if (currentPage < 1 || currentPage > totalPages) {
-//       navigate('/404'); // Redirect to the 404 page
-//     } else {
-//       fetchObjectsForPage();
-//     }
-//   }, [currentPage, searchTerm, navigate, totalPages]);
-
-//   const visibleObjects = objects.slice(0, 50); // Display the first 50 objects from the search results
+//   const visibleObjects = objects.slice(0, 50); 
 
 //   const handleFirstPage = () => {
 //     setCurrentPage(1);
 //   };
 
 //   const handleNextPage = () => {
-//     setCurrentPage((prevPage) => prevPage + 1);
+//     const nextPage = currentPage + 1;
+//     setCurrentPage(nextPage);
+//     const url = `/collection/page/${nextPage}${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`;
+//     navigate(url);
 //   };
-
+  
 //   const handlePreviousPage = () => {
-//     setCurrentPage((prevPage) => prevPage - 1);
+//     if (currentPage > 1) {
+//       const prevPage = currentPage - 1;
+//       setCurrentPage(prevPage);
+//       const url = `/collection/page/${prevPage}${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`;
+//       navigate(url);
+//     }
 //   };
-
+  
 //   const handleLastPage = () => {
 //     setCurrentPage(totalPages);
 //   };
 
 //   const handleSearch = (value) => {
-//     console.log('Search Term:', value);
 //     setSearchTerm(value);
+//     navigate(`/collection/page/1${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}&searchTerm=${value}`);
 //     setCurrentPage(1);
 //   };
+  
 
 //   return (
 //     <div>
-//       <div className="pagination-buttons">
-//         {currentPage > 1 && <Link to={`/collection/page/1`} onClick={handleFirstPage}>First Page</Link>}
-//         {currentPage > 1 && <Link to={`/collection/page/${currentPage - 1}`} onClick={handlePreviousPage}>Previous Page</Link>}
-//         {currentPage < totalPages && <Link to={`/collection/page/${currentPage + 1}`} onClick={handleNextPage}>Next Page</Link>}
-//         {currentPage !== totalPages && <Link to={`/collection/page/${totalPages}`} onClick={handleLastPage}>Last Page</Link>}
-//       </div>
-//       <div className="collection-page">
-//         <h1>Collections - Page {currentPage}</h1>
-//         <SearchArts searchValue={handleSearch} />
+//     <div className="pagination-buttons">{currentPage > 1 && (
+//       <Link to={`/collection/page/1${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`}
+//        onClick={handleFirstPage}>
+//        First Page
+//       </Link>
+//  )}
+//   {currentPage > 1 && (
+//     <Link
+//       to={`/collection/page/${currentPage - 1}${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`}
+//       onClick={handlePreviousPage}>
+//       Previous Page
+//     </Link>
+//   )}
+//   {currentPage < totalPages && (
+//     <Link
+//       to={`/collection/page/${currentPage + 1}${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`}
+//       onClick={handleNextPage}>
+//       Next Page
+//     </Link>
+//   )}
+//   {currentPage !== totalPages && (
+//     <Link
+//       to={`/collection/page/${totalPages}${isDepartmentSearch ? `?departmentIds=${departmentId}` : ''}${searchTerm ? `&searchTerm=${searchTerm}` : ''}`}
+//       onClick={handleLastPage}>
+//      Last Page
+//     </Link>
+//   )}
+// </div>
+//     {isDepartmentSearch && <h1>Department {departmentId} - Page {currentPage}</h1>}
+//     {!isDepartmentSearch && searchTerm && (
+// <div className="collection-page">
+//     <h1>Search Results for "{searchTerm}" - Page {currentPage}</h1>
+//     <SearchArts searchValue={handleSearch} />
+// </div>
+// )}
+//     {!isDepartmentSearch && !searchTerm && (
+//   <div className="collection-page">
+//     <h1>Art Collections - Page {currentPage}</h1>
+//     <SearchArts searchValue={handleSearch} />
+//   </div>
+// )}
 
-//         {loading && <p>Loading...</p>}
-//         {!loading && visibleObjects.length === 0 && <p>No objects found.</p>}
-//         {!loading && visibleObjects.length > 0 && (
-//           <div>
-//             <Grid container spacing={2}>
-//               {visibleObjects.map((objectID) => (
-//                 <ArtListCard key={objectID} objectID={objectID} />
-//               ))}
-//             </Grid>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
+//   {!loading && visibleObjects.length === 0 && (<p style={{ fontSize: '25px', fontWeight: 'bold' }}>No objects found.</p>)}
+//   {!loading && visibleObjects.length > 0 && (
+//   <div>
+//       <Grid container spacing={2}>
+//         {visibleObjects.map((objectID) => (
+//         <ArtListCard key={objectID} objectID={objectID} /> ))}
+//       </Grid>
+//   </div>
+//     )}
+//   </div>
+// );
 // };
 
 // export default CollectionPage;
@@ -158,11 +218,13 @@ const isDepartmentSearch = !!departmentId;
 
         setLoading(true);
         let apiUrl = '';
+        let objectsPerPage = 50;
         if (departmentId) {
         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}&offset=${(currentPage - 1) * 50}`;
         } else {
         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects?offset=${(currentPage - 1) * 50}`;
         if (searchTerm !== undefined && searchTerm !== null && searchTerm !== '') {
+         objectsPerPage = 20;
         apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${(searchTerm)}`;
          }
         }
@@ -181,11 +243,11 @@ const isDepartmentSearch = !!departmentId;
         setTotalPages(calculatedTotalPages);
 
       
-        const startIndex = (currentPage - 1) * 50;
-        const endIndex = Math.min(startIndex + 50, totalObjects);
-        const objectsForCurrentPage = allObjects.slice(startIndex, endIndex);
+        const startIndex = (currentPage - 1) * objectsPerPage;
+        const endIndex = startIndex + objectsPerPage;
+        const visibleObjects = allObjects.slice(startIndex, endIndex);
 
-        setObjects(objectsForCurrentPage);
+        setObjects(visibleObjects);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching objects:', error);
