5695efac3d8a77ca8fb44391ee3e5f39fa5f0adb
Sachin Devangan <dsachin.98@gmail.com>
2023-10-30 00:15:25 -0400

    enter button for search

--- Lab 4/Metropolitan-Museum-of-Art/src/components/SearchArts.jsx
+++ Lab 4/Metropolitan-Museum-of-Art/src/components/SearchArts.jsx
@@ -1,15 +1,37 @@
+// import React from 'react';
+
+// const SearchArts = (props) => {
+//   const handleChange = (e) => {
+//     props.searchValue(e.target.value); 
+//   };
+
+//   return (
+//     <form method='POST' onSubmit={(e) => e.preventDefault()} name='formName' className='center'>
+//       <label style={{ fontWeight: 'bold' }}>
+//         <span>Search Arts: </span>
+//         <input autoComplete='off' type='text' name='searchTerm' onChange={handleChange} />
+//       </label>
+//     </form>
+//   );
+// };
+
+// export default SearchArts;
+
+
 import React from 'react';
 
 const SearchArts = (props) => {
-  const handleChange = (e) => {
-    props.searchValue(e.target.value); 
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    
+    props.searchValue(e.target.searchTerm.value);
   };
 
   return (
-    <form method='POST' onSubmit={(e) => e.preventDefault()} name='formName' className='center'>
+    <form onSubmit={handleSubmit} name='formName' className='center'>
       <label style={{ fontWeight: 'bold' }}>
         <span>Search Arts: </span>
-        <input autoComplete='off' type='text' name='searchTerm' onChange={handleChange} />
+        <input autoComplete='off' type='text' name='searchTerm' />
       </label>
     </form>
   );
