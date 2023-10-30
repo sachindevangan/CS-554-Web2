d7e9ff8fa9471ce7cd9a664649439d7eee049d94
Sachin Devangan <dsachin.98@gmail.com>
2023-10-30 15:34:05 -0400

    Trim input and press enter to search

--- Lab 4/Metropolitan-Museum-of-Art/src/components/SearchArts.jsx
+++ Lab 4/Metropolitan-Museum-of-Art/src/components/SearchArts.jsx
@@ -18,13 +18,39 @@
 // export default SearchArts;
 
 
+// import React from 'react';
+
+// const SearchArts = (props) => {
+//   const handleSubmit = (e) => {
+//     e.preventDefault(); 
+    
+//     props.searchValue(e.target.searchTerm.value);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} name='formName' className='center'>
+//       <label style={{ fontWeight: 'bold' }}>
+//         <span>Search Arts: </span>
+//         <input autoComplete='off' type='text' name='searchTerm' />
+//       </label>
+//     </form>
+//   );
+// };
+
+// export default SearchArts;
+
 import React from 'react';
 
 const SearchArts = (props) => {
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     
-    props.searchValue(e.target.searchTerm.value);
+   
+    const searchTerm = e.target.searchTerm.value.trim();
+
+    if (searchTerm !== '') {
+      props.searchValue(searchTerm);
+    }
   };
 
   return (
