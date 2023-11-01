ab4b339dbbc8c2246f0495a0a6c4f65629f65521
Sachin Devangan <dsachin.98@gmail.com>
2023-10-31 20:20:45 -0400

    HTML VALIDATION

--- Lab 4/Metropolitan-Museum-of-Art/src/components/Art.jsx
+++ Lab 4/Metropolitan-Museum-of-Art/src/components/Art.jsx
@@ -78,85 +78,85 @@ const Art = (props)  =>{
         <Typography
           variant='body2'
           color='textSecondary'
-          component='span'
+          component='div'
           sx={{
             borderBottom: '1px solid #000000',
             fontWeight: 'bold'
           }}
         >
-          <dl>
-            <p>
+          <dl className='description-list'>
+            <div>
               <dt className='title'>Artist Name</dt>
               <dd>{artData.artistDisplayName || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Artist Bio:</dt>
               <dd>{artData.artistDisplayBio || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Artist Gender:</dt>
               <dd>{artData.artistGender || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Object Date:</dt>
               <dd>{artData.objectDate || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Department:</dt>
               <dd>{artData.department || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Medium:</dt>
               <dd>{artData.medium || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Classification:</dt>
               <dd>{artData.classification || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Culture:</dt>
               <dd>{artData.culture || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Dimensions:</dt>
               <dd>{artData.dimensions || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Period:</dt>
               <dd>{artData.period || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Credit Line:</dt>
               <dd>{artData.creditLine || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Geography Type:</dt>
               <dd>{artData.geographyType || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Repository:</dt>
               <dd>{artData.repository || 'N/A'}</dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Object URL:</dt>
               <dd>
                 <a href={artData.objectURL} target='_blank' rel='noopener noreferrer'>
                   {artData.objectURL || 'N/A'}
                 </a>
               </dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Wikidata URL:</dt>
               <dd>
                 <a href={artData.objectWikidata_URL} target='_blank' rel='noopener noreferrer'>
                   {artData.objectWikidata_URL || 'N/A'}
                 </a>
               </dd>
-            </p>
-            <p>
+            </div>
+            <div>
               <dt className='title'>Gallery Number:</dt>
               <dd>{artData.GalleryNumber || 'N/A'}</dd>
-            </p>
+            </div>
           </dl>
           <Link to='/collection/page/1'>Back to all artworks...</Link>
         </Typography>
