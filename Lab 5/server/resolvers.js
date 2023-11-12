4eead881e0bbf81e63e383193af8a5e413303417
Sachin Devangan <dsachin.98@gmail.com>
2023-11-11 22:14:08 -0500

    resolver done

--- Lab 5/server/resolvers.js
+++ Lab 5/server/resolvers.js
@@ -80,4 +80,67 @@ export const resolvers = {
             }
           },
         },
-};
+
+        comicsPage: async (_, { pageNum }) => {
+          if (!client.isOpen) {
+            await client.connect();
+          }
+      
+          // Check if the paginated data exists in the Redis cache
+          const exist = await client.exists(`comicsPage:${pageNum}`);
+          if (exist) {
+            const comicsPageRedisData = await client.get(`comicsPage:${pageNum}`);
+            return res.status(200).json(JSON.parse(comicsPageRedisData));
+          }
+      
+          // Fetch paginated comic data from the Marvel API
+          const publicKey = '9f21335888c4dcdcf185e6ec54d57098';
+          const privateKey = '9835ef68c5da41d4945e66ad688ea918e31d4ba5';
+          const ts = new Date().getTime();
+          const stringToHash = ts + privateKey + publicKey;
+          const hash = md5(stringToHash);
+          const baseUrl = 'https://gateway.marvel.com:443/v1/public/comics';
+          const url = `${baseUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}&offset=${pageNum * 20}&limit=20`;
+      
+          try {
+            const response = await axios.get(url);
+      
+            // Check for successful response
+            if (response.data && response.data.data && response.data.data.results.length > 0) {
+              const comicsPageData = response.data.data.results.map((comicData) => ({
+                id: comicData.id,
+                title: comicData.title,
+                description: comicData.description,
+                issueNumber: comicData.issueNumber,
+                variantDescription: comicData.variantDescription,
+                pageCount: comicData.pageCount,
+                modified: comicData.modified,
+                resourceURI: comicData.resourceURI,
+                series: {
+                  name: comicData.series.name,
+                },
+                thumbnail: {
+                  path: comicData.thumbnail.path,
+                  extension: comicData.thumbnail.extension,
+                },
+              }));
+      
+              // Store the paginated data in Redis cache
+              const data = await client.set(`comicsPage:${pageNum}`, JSON.stringify(comicsPageData));
+      
+              // Return the paginated data
+              return comicsPageData;
+            } else {
+              // Throw a GraphQL error indicating that the page doesn't exist or has no comics
+              throw new GraphQLError('Page Not Found', {
+                extensions: { code: 'NOT_FOUND', statusCode: 404 }
+              });
+            }
+          } catch (error) {
+            console.error('Error fetching data from Marvel API:', error);
+            throw new GraphQLError('Internal Server Error', {
+              extensions: { code: 'INTERNAL_SERVER_ERROR' }
+            });
+          }
+        },
+      };
